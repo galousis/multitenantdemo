12e0c9a6fff06f72099b4cfea59d617e51afc84b
Thanos Theodorakopoulos <thanos@square-medias.com>
2017-05-10 11:30:33 +0300

    Handle notfoundhttp exception.

--- app/Application/Exceptions/Handler.php
+++ app/Application/Exceptions/Handler.php
@@ -5,6 +5,7 @@ namespace App\Application\Exceptions;
 use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -44,7 +45,15 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+		if ($this->isHttpException($exception))
+		{
+			if($exception instanceof NotFoundHttpException)
+			{
+				return response()->view('exceptions.404', [], 404);
+			}
+			return $this->renderHttpException($exception);
+		}
+		return parent::render($request, $exception);
     }
 
     /**
