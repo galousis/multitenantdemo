a50d0e81cb8c51c720b0644bbb311e1b34c41f92
Thanos Theodorakopoulos <thanos@square-medias.com>
2017-05-11 22:24:56 +0300

    Created Authentifier.

---
+++ app/Domain/User/Authentifier.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Domain\User;
+
+use App\Domain\User\Contracts\UserRepositoryContract;
+use App\Domain\Event\DomainEvent;
+
+abstract class Authentifier
+{
+	#region properties
+	/**
+	 * @var UserRepositoryContract
+	 */
+	private $repository;
+	#endregion
+
+	#region Constructor
+	/**
+	 * @param string $repository
+	 */
+	public function __construct($repository)
+	{
+		$this->repository = $repository;
+	}
+	#endregion
+
+	#region Methods
+	public function authenticate($email, $password)
+	{
+
+		$user = $this->repository->ofEmail($email);
+
+		if (!$user) {
+			return false;
+		}
+		if ($user->password() !== $password) {
+			return false;
+		}
+		$this->persistAuthentication($user);
+		return true;
+	}
+
+	abstract public function logout();
+	abstract protected function persistAuthentication(User $user);
+	abstract protected function isAlreadyAuthenticated();
+	#endregion
+
+}
