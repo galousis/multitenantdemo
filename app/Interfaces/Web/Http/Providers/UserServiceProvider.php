a2d07b2f050095634f68a35fd316210bdef50793
Thanos Theodorakopoulos <thanos@square-medias.com>
2017-06-01 22:21:57 +0300

    Seed operateion for media, not finished. Binded contracts with implementation for tour, destinations.

--- app/Interfaces/Web/Http/Providers/UserServiceProvider.php
+++ app/Interfaces/Web/Http/Providers/UserServiceProvider.php
@@ -19,6 +19,8 @@ class UserServiceProvider extends ServiceProvider
 	public function register()
 	{
 		$this->app->bind('App\Domain\User\Contracts\UserRepositoryContract', 'App\Infrastructure\Doctrine\Repositories\UserRepository');
+		$this->app->bind('App\Domain\Destination\Contracts\DestinationRepositoryContract', 'App\Infrastructure\Doctrine\Repositories\DestinationRepository');
+		$this->app->bind('App\Domain\Tour\Contracts\TourRepositoryContract', 'App\Infrastructure\Doctrine\Repositories\TourRepository');
 		$this->app->bind('App\Application\Services\DataTransformer\User\UserDataTransformer', 'App\Application\Services\DataTransformer\User\UserTransformer');
 	}
 }
