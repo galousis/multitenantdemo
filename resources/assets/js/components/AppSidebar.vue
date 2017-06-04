f472f9a80c5337149a52d9586c66f0ea3e3af5dd
Thanos Theodorakopoulos <thanos@square-medias.com>
2017-06-04 20:28:48 +0300

    Changed th sidebar menu, using elementio menu.

--- resources/assets/js/components/AppSidebar.vue
+++ resources/assets/js/components/AppSidebar.vue
@@ -1,27 +1,16 @@
 <template>
     <div>
-
         <!-- Left side column. contains the logo and sidebar -->
         <aside class="main-sidebar">
             <!-- sidebar: style can be found in sidebar.less -->
             <section class="sidebar">
                 <!-- Sidebar Menu -->
-                <ul class="sidebar-menu">
-                    <li class="header">TOOLS</li>
-                    <li>
-                        <router-link to="/"><i class="fa fa-desktop"></i> Dashboard</router-link>
-                    </li>
-                    <li>
-                        <router-link to="/users"><i class="fa fa-desktop"></i> Users </router-link>
-                    </li>
-                    <li>
-                        <router-link to="/tours"><i class="fa fa-desktop"></i> Tours </router-link>
-                    </li>
-                    <li>
-                        <router-link to="/destinations"><i class="fa fa-desktop"></i> Destinations </router-link>
-                    </li>
-
-                </ul>
+                <el-menu class="el-menu-vertical-demo">
+                    <el-menu-item index="1" @click="openPage"><i class="el-icon-menu"></i>Dashboard</el-menu-item>
+                    <el-menu-item index="2" @click="openPage"><i class="el-icon-menu"></i>Users</el-menu-item>
+                    <el-menu-item index="3" @click="openPage"><i class="el-icon-menu"></i>Tours</el-menu-item>
+                    <el-menu-item index="4" @click="openPage"><i class="el-icon-setting"></i>Destinations</el-menu-item>
+                </el-menu>
                 <!-- /.sidebar-menu -->
             </section>
             <!-- /.sidebar -->
@@ -32,13 +21,39 @@
 <script>
     export default {
         ready() {
-            console.log('Component ready.')
+            //console.log('Component ready.')
         },
         data(){
             return {
                 message: "side Bar "
             }
 
+        },
+        methods: {
+            openPage : function (event) {
+
+//                if (event) {
+//                    console.log(event.$el.innerText);
+//                }
+
+                switch(event.$el.innerText) {
+                    case 'Dashborad':
+                        this.$router.push({path: '/'});
+                        break;
+                    case 'Users':
+                        this.$router.push({path: '/users'});
+                        break;
+                    case 'Tours':
+                        this.$router.push({path: '/tours'});
+                        break;
+                    case 'Destinations':
+                        this.$router.push({path: '/destinations'});
+                        break;
+                    default:
+                        this.$router.push({path: '/'});
+                        break;
+                }
+            }
         },
         computed: {
             demo: function () {
--- resources/assets/js/components/Destinations/Index.vue
+++ resources/assets/js/components/Destinations/Index.vue
@@ -5,36 +5,116 @@
                 <h1>Destinations</h1>
             </div>
         </header>
+
         <!-- Main content -->
         <section class="main-content">
-            <tabs>
-                <tab name="Users" title="Users" active=true>
-                    <div id="users">
-                        <v-server-table url="/api/v1/destinations" :columns="columns" :options="options"></v-server-table>
-                    </div>
-                </tab>
-            </tabs>
+
+            <el-table
+                    :data="tableData"
+                    :default-sort = "{prop: 'id', order: 'ascending'}"
+                    border
+                    style="width: 100%">
+                <el-table-column
+                        label="ID"
+                        prop="id"
+                        sortable
+                        width="180">
+                    <template scope="scope">
+                        <span style="margin-left: 10px">{{ scope.row.id}}</span>
+                    </template>
+                </el-table-column>
+                <el-table-column
+                        label="Name"
+                        width="180">
+                    <template scope="scope">
+                        <el-popover trigger="hover" placement="top">
+                            <p>Name: {{ scope.row.name }}</p>
+                            <div slot="reference" class="name-wrapper">
+                                <el-tag>{{ scope.row.name }}</el-tag>
+                            </div>
+                        </el-popover>
+                    </template>
+                </el-table-column>
+                <el-table-column
+                        label="Operations">
+                    <template scope="scope">
+                        <el-button
+                                size="small"
+                                @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
+                        <el-button
+                                size="small"
+                                type="danger"
+                                @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+            <!-- Pagination block -->
+            <div class="block">
+                <div class="pagination">
+                    <button class="btn btn-default" @click="fetchTours(paginationT.prev_page_url)" :disabled="!paginationT.prev_page_url">
+                        Previous
+                    </button>
+                    <span>Page {{paginationT.current_page}} of {{paginationT.last_page}}</span>
+                    <button class="btn btn-default" @click="fetchTours(paginationT.next_page_url)" :disabled="!paginationT.next_page_url">Next
+                    </button>
+                </div>
+            </div>
+
         </section>
+
+
     </div>
 </template>
+
 <script>
-    import tabs from '../../components/Util/Tabs.vue'
-    import tab from '../../components/Util/Tab.vue'
     export default {
         data() {
             return {
-                columns: ['id', 'name'],
-                options: {
-                    templates: {
-                        erase: function (h, row) {
-                            return ''
-                        }
-                    }
-                }
+                tableData: [],
+                paginationT: {}
             }
         },
-        components: {
-            tabs, tab
+        created: function () {
+            this.fetchTours()
+        },
+        methods: {
+            handleEdit(index, row) {
+                console.log(index, row);
+            },
+            handleDelete(index, row) {
+                console.log(index, row);
+            },
+            handleSizeChange(val) {
+                console.log(`${val} items per page`);
+            },
+            handleCurrentChange(val) {
+                console.log(`current page: ${val}`);
+            },
+            fetchTours: function (page_url) {
+                let vm = this;
+
+                page_url = page_url || '/api/v1/tours';
+
+                return this.$http.get(page_url).then((response) => {
+
+                    vm.tableData = response.data.data;
+                    vm.paginationT = vm.makePagination(response.data);
+
+                    console.log(response.data);
+                });
+            },
+            makePagination: function (data){
+                //here we use response.data
+                var pagination = {
+                    current_page: data.current_page,
+                    last_page: data.last_page,
+                    next_page_url: data.next_page_url,
+                    prev_page_url: data.prev_page_url
+                }
+                return pagination;
+
+            }
         }
     }
 </script>
+
